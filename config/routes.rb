8f8bf35e55cea5aa566dadb08c444255803dc9a8
NickyEXE <dovern42@gmail.com>
2019-11-05 12:11:25 -0500

    added logout route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   get '/login', to: "auth#new", as: 'login'
   post '/login', to: "auth#create", as: 'authenticate'
+  delete '/login', to: "auth#logout", as: 'logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
