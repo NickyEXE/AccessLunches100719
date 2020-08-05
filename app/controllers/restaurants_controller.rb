7c11ed15db9f0791a1601d415863edfb07161b67
NickyEXE <dovern42@gmail.com>
2020-08-04 20:22:10 -0400

    scopes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,11 +3,9 @@ class RestaurantsController < ApplicationController
 
   def index
     @location_query = params[:location_query]
-    if @location_query
-      @restaurants = Restaurant.get_restaurants_by_location(params[:location_query])
-    else
-      @restaurants = Restaurant.filter_by_type(params[:query]).sort_by_type(params[:Filter])
-    end
+    @restaurants = Restaurant.get_restaurants_by_location(params[:location_query])
+      .filter_by_type(params[:query])
+      .sort_by_type(params[:Filter])
   end
 
   def show
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,14 @@ class Restaurant < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
 
+  scope :order_by_name, -> {order(:name)}
+  scope :filter_by_type, -> (query){where("kind_of_food LIKE ?", "%#{query}%")}
+
+
+  # def self.filter_by_type(query)
+  #   where("kind_of_food LIKE ?", "%#{query}%")
+  # end
+
   def self.find_or_create_business_from_yelp(business)
     find_or_create_by(
       name: business["name"],
@@ -14,14 +22,14 @@ class Restaurant < ApplicationRecord
       )
   end
 
+  # def self.order_by_name
+  #   order(:name)
+  # end
+
   def average_rating
     unless reviews.empty?
       reviews.average(:rating)
     end
-  end
-
-  def self.filter_by_type(query)
-    where("kind_of_food LIKE ?", "%#{query}%")
   end
 
   def self.sort_by_type(selected_type)
@@ -36,8 +44,16 @@ class Restaurant < ApplicationRecord
   end
 
   def self.get_restaurants_by_location(location)
+    return all if location.blank?
+    # get up to 50 restaurants from yelp
     search = YelpSearch.new(location).results
-    search["businesses"].map{|business| Restaurant.find_or_create_business_from_yelp(business)}
+    # map through them, and find or create each restaurant that relates to the business we got from yelp
+    if search["error"]
+      restaurants = []
+    else
+      restaurants = search["businesses"].map{|business| Restaurant.find_or_create_business_from_yelp(business)}
+    end
+    Restaurant.where(id: restaurants.pluck(:id))
   end
 
 
